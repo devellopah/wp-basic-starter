d7ca2bbcef23bef694780618ccfac50eeac5efa7
Islam Ibakaev <islamibakaev@gmail.com>
2025-01-12 15:26:50 +0300

    added header and footer

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -8,27 +9,198 @@
  *
  * @package Basic_Starter
  */
-
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'basic-starter' ) ); ?>">
+</div>
+<!-- end of content -->
+<?php if (!is_404()) : ?>
+	<!-- footer -->
+	<footer class="footer" id="footer">
+		<div class="container">
+			<div class="footer__main">
+				<div class="c-socnav footer__socnav">
+					<ul class="c-socnav__list">
+						<?php foreach (get_field('socials', 'option') as $item) : ?>
+							<li class="c-socnav__item">
+								<a href="<?= esc_url($item['url']) ?>" title="<?= esc_attr($item['title']) ?>" target="_blank" class="c-socnav__link">
+									<?php if ($item['icon']) : ?>
+										<img class="c-socnav__icon" src="<?= esc_url($item['icon']['url']) ?>" alt="<?= esc_attr($item['icon']['alt']) ?>">
+									<?php endif; ?>
+								</a>
+							</li>
+						<?php endforeach ?>
+					</ul>
+				</div>
+				<a href="index.html" class="footer__logotype">
+					<img src="<?= esc_url(get_field('footer_logo_desc_1', 'option')['url']) ?>" alt="<?= esc_attr(get_field('footer_logo_desc_1', 'option')['alt']) ?>" width="154" height="96">
+				</a>
 				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'basic-starter' ), 'WordPress' );
+				wp_nav_menu(
+					array(
+						'theme_location' => 'menu-1',
+						// 'items_wrap' => '<ul class="%2$s">%3$s</ul>',
+						'items_wrap' => '<ul class="%2$s"><li class="footer-nav__item"><a href="' . home_url() . '" class="footer-nav__link">Главная</a></li>%3$s</ul>',
+						'menu_class' => 'footer-nav__list',
+						'container' => 'nav',
+						'container_class' => 'footer-nav footer__nav',
+						'fallback_cb' => '__return_empty_string',
+						'walker' => new Basic_Starter_Footer_Menu_Walker()
+					)
+				);
 				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'basic-starter' ), 'basic-starter', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+				<div class="footer-contacts footer__contacts">
+					<ul class="footer-contacts__list">
+						<li class="footer-contacts__item">
+							<a href="tel:<?php mw_tel_sanitized(get_field('footer_tel_1', 'option')) ?>" class="footer-contacts__link"><?= esc_html(get_field('footer_tel_1', 'option')) ?></a>
+						</li>
+						<li class="footer-contacts__item">
+							<a href="mailto:<?php bloginfo('admin_email') ?>" class="footer-contacts__link"><?php bloginfo('admin_email') ?></a>
+						</li>
+						<li class="footer-contacts__item">
+							<address class="footer-contacts__link"><?= esc_html(get_field('city', 'option')) ?> <?= esc_html(get_field('street', 'option')) ?> <?= esc_html(get_field('building', 'option')) ?></address>
+						</li>
+					</ul>
+				</div>
+				<div class="c-subscribe footer__subscribe">
+					<span class="c-subscribe__title"><?php the_field('newsletter_heading', 'option') ?></span>
+					<?= do_shortcode('[wp_email_capture_form]'); ?>
+				</div>
+			</div>
+			<div class="footer__bottom">
+				<p class="footer__copyright"><?php the_field('footer_copyright', 'option') ?></p>
+				<a href="<?= esc_url(get_field('footer_agency', 'option')['link']) ?>" class="footer__dev" target="_blank">
+					<img src="<?= esc_url(get_field('footer_agency', 'option')['image']['url']) ?>" alt="<?= esc_attr(get_field('footer_agency', 'option')['image']['alt']) ?>">
+				</a>
+			</div>
+		</div>
+	</footer>
+	<!-- end of footer -->
+<?php endif; ?>
+</div>
+<!-- end of wrapper -->
+
+<!-- menu -->
+<div class="menu" id="menu">
+	<div class="menu__wrap">
+		<?php
+		wp_nav_menu(
+			array(
+				'theme_location' => 'menu-1',
+				'items_wrap' => '<ul class="%2$s"><li class="nav__item"><a href="' . home_url() . '" class="nav__link">Главная</a></li>%3$s</ul>',
+				'menu_class' => 'nav__list',
+				'container' => 'nav',
+				'container_class' => 'nav menu__nav',
+				'fallback_cb' => '__return_empty_string',
+				'walker' => new Basic_Starter_Primary_Menu_Walker()
+			)
+		);
+		?>
+		<a href="tel:<?php mw_tel_sanitized(get_field('footer_tel_1', 'option')) ?>" class="menu__link"><?= esc_html(get_field('footer_tel_1', 'option')) ?></a>
+	</div>
+</div>
+<!-- end of menu -->
+
+<!-- modals -->
+<div class="modal" id="modal-application">
+	<div class="modal__wrap">
+		<button class="modal__close-btn modal__close" aria-label="Закрыть">
+			<svg>
+				<use xlink:href="<?= THEME_URI ?>/assets/img/sprite.svg#icon-close"></use>
+			</svg>
+		</button>
+		<div class="application modal__application js-form-wrapper">
+			<div class="application__wrap">
+				<?= get_template_part('components/form', null, ['namespace' => 'application']) ?>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- end of modals -->
+
+<!-- popups -->
+<div class="popup js-popup" id="popup-application-success">
+	<div class="popup__wrap">
+		<div class="popup__wrapper">
+			<picture class="popup__icon">
+				<img src="<?= esc_url(get_field('submit_success', 'option')['image']['url']) ?>" alt="<?= esc_attr(get_field('submit_success', 'option')['image']['alt']) ?>">
+			</picture>
+			<span class="popup__title"><?= esc_html(get_field('submit_success', 'option')['heading']) ?></span>
+			<span class="popup__description"><?= wp_kses_post(get_field('submit_success', 'option')['description']) ?></span>
+			<button class="popup__btn btn js-popup-closer"><?= esc_html(get_field('submit_success', 'option')['btn_text']) ?></button>
+		</div>
+	</div>
+</div>
+<div class="popup js-popup" id="popup-application-fail">
+	<div class="popup__wrap">
+		<div class="popup__wrapper">
+			<picture class="popup__icon">
+				<img src="<?= esc_url(get_field('submit_fail', 'option')['image']['url']) ?>" alt="<?= esc_attr(get_field('submit_fail', 'option')['image']['alt']) ?>">
+
+			</picture>
+			<span class="popup__title"><?= esc_html(get_field('submit_fail', 'option')['heading']) ?></span>
+			<span class="popup__description"><?= wp_kses_post(get_field('submit_fail', 'option')['description']) ?></span>
+			<button class="popup__btn btn js-popup-closer"><?= esc_html(get_field('submit_fail', 'option')['btn_text']) ?></button>
+		</div>
+	</div>
+</div>
+<!-- end of popups -->
+
+<?php if (function_exists('wp_email_capture_form')) : ?>
+	<div class="popup" id="popup-subscribe-error" data-placeholder_email="<?= the_field('newsletter_placeholder_email', 'option') ?>" data-error_user="<?= the_field('newsletter_error_user', 'option') ?>" data-error_email="<?= the_field('newsletter_error_email', 'option') ?>">
+		<div class="popup__wrap">
+			<div class="popup__wrapper">
+				<picture class="popup__icon">
+					<img src="<?= THEME_URI ?>/assets/img/icon-error.svg" alt="">
+				</picture>
+				<span class="popup__title"><?= the_field('newsletter_error_heading', 'option') ?></span>
+				<span class="popup__description js-newsletter-error"></span>
+				<button id="to-newsletter-scroller" class="popup__btn btn"><?= the_field('newsletter_error_btn_text', 'option') ?></button>
+			</div>
+		</div>
+	</div>
+
+	<script>
+		(function($) {
+			$body = $(document.body)
+			$newsletterModal = $body.find('#popup-subscribe-error')
+			$error = $body.find('.wp-email-capture-error')
+			if ($error.length) {
+				console.log('error', $error.text())
+				$errorText = $error.text()
+				if ($errorText.toLowerCase().includes('email')) {
+					$errorText = $newsletterModal.data('error_email')
+				} else if ($errorText.toLowerCase().includes('user')) {
+					$errorText = $newsletterModal.data('error_user')
+				}
+
+				$body.addClass('scroll-disabled')
+				$newsletterModal.addClass('is--active')
+				$body.find('.js-newsletter-error').text($errorText)
+				$error.remove()
+
+				$('#to-newsletter-scroller').on('click', function() {
+					$body.removeClass('scroll-disabled')
+					$newsletterModal.removeClass('is--active')
+
+					$('#wp_email_capture_2')[0].scrollIntoView({
+						behavior: 'instant'
+					})
+				})
+			}
+			$form = $('[name=wp_email_capture_display]')
+			$form.addClass('c-subscribe__form').wrapInner('<div class="c-subscribe__inner"></div>')
+			$form.find('br').remove()
+			$form.find('.wp-email-capture-email-label').remove()
+			$form.find('.wp-email-capture-email-input').addClass('c-subscribe__input').prop('placeholder', $newsletterModal.data('placeholder_email'))
+			$form.find('.wp-email-capture-name').hide()
+			$form.find('[type=submit]').replaceWith('<button name="Submit" type="submit" class="wp-email-capture-submit wp-email-capture-widget-worldwide c-subscribe__btn" aria-label="Отправить"> <svg><use xlink: href = "<?= THEME_URI ?>/assets/img/sprite.svg#icon-plus" > < /use> </svg> </button>')
+		})(jQuery)
+	</script>
+
+<?php endif ?>
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -12,8 +13,9 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
@@ -21,39 +23,43 @@
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'basic-starter' ); ?></a>
+	<?php wp_body_open(); ?>
+	<!-- wrapper -->
+	<div class="wrapper">
+		<!-- header -->
+		<header id="header" class="header">
+			<div class="container">
+				<div class="header__wrapper">
+					<a href="<?= home_url() ?>" class="header__logotype">
+						<img class="default" src="<?= esc_url(get_field('header_logo_desc_1', 'option')['url']) ?>" alt="<?= esc_attr(get_field('header_logo_desc_1', 'option')['alt']) ?>" width="100" height="62">
+					</a>
+					<?php
+					if (!is_404()) :
+						wp_nav_menu(
+							array(
+								'theme_location' => 'menu-1',
+								'items_wrap' => '<ul class="%2$s">%3$s</ul>',
+								'menu_class' => 'nav__list',
+								'container' => 'nav',
+								'container_class' => 'nav header__nav',
+								'fallback_cb' => '__return_empty_string',
+								'walker' => new Basic_Starter_Primary_Menu_Walker()
+							)
+						);
+					?>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$basic_starter_description = get_bloginfo( 'description', 'display' );
-			if ( $basic_starter_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $basic_starter_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+						<a href="tel:<?php mw_tel_sanitized(get_field('header_tel_1', 'option')) ?>" class="header__link"><?= esc_html(get_field('header_tel_1', 'option')) ?></a>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'basic-starter' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+						<button class="hamburger" id="hamburger-toggle" aria-label="Меню">
+							<span class="hamburger__inner"></span>
+							<span class="hamburger__inner"></span>
+						</button>
+
+					<?php endif; ?>
+
+				</div>
+			</div>
+		</header>
+		<!-- end of header -->
+		<!-- content -->
+		<div class="wrapper__content">
