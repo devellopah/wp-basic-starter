4865c507180a60e717887a965f538620eaefa5a8
Islam Ibakaev <islamibakaev@gmail.com>
2025-04-03 22:01:33 +0300

    remove custom-header and custom-logo

--- functions.php
+++ functions.php
@@ -72,35 +72,8 @@ function basic_starter_setup()
 		)
 	);
 
-	// Set up the WordPress core custom background feature.
-	add_theme_support(
-		'custom-background',
-		apply_filters(
-			'basic_starter_custom_background_args',
-			array(
-				'default-color' => 'ffffff',
-				'default-image' => '',
-			)
-		)
-	);
-
 	// Add theme support for selective refresh for widgets.
 	add_theme_support('customize-selective-refresh-widgets');
-
-	/**
-	 * Add support for core custom logo.
-	 *
-	 * @link https://codex.wordpress.org/Theme_Logo
-	 */
-	add_theme_support(
-		'custom-logo',
-		array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
-		)
-	);
 }
 add_action('after_setup_theme', 'basic_starter_setup');
 
@@ -197,11 +170,6 @@ function basic_starter_scripts()
 	}
 }
 add_action('wp_enqueue_scripts', 'basic_starter_scripts');
-
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.
